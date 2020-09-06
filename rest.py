6cdd85aef83599b70bdce2b736c254850320b448
Isaque Moura <improgramcode@gmail.com>
2020-09-06 15:15:08 -0300

    Criação de um serviço rest para test

---
+++ rest.py
@@ -0,0 +1,16 @@
+import requests
+
+def consulta():
+    r = requests.get('https://jsonplaceholder.typicode.com/posts/')
+    print(r.status_code)
+    print(r.json())
+    for posts in r.json():
+        print(posts['userId'], posts['id'], posts['title'], posts['body'])
+
+def insere():
+    title = 'Testando service rest'
+    body = 'Testando corpo service rest'
+    posts = {"title": title, "body": body}
+    r = requests.post('https://jsonplaceholder.typicode.com/posts/', json=posts)
+    print(r.status_code)
+    print(r.json())        
